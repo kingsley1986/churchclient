e6913eaf92bca771d526d54a3f5ac1b9b99b30a7
Kingsley <chukwumakingsley1@gmail.com>
2020-10-04 17:28:52 +0200

    Images sliding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from "axios";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import PostsList from "./components/posts-list.component";
@@ -14,110 +16,189 @@ import image1 from "./images/one.jpg";
 import "react-animated-slider/build/horizontal.css";
 import "normalize.css/normalize.css";
 import "./components/homepage-slider.css";
+import ProductCard from "./components/product";
+import Slider2 from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Card } from "react-bootstrap";
 
-class App extends Component {
-  render() {
-    const content = [
+const App = (props) => {
+  const [slides, setSlides] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://cryptic-shelf-72177.herokuapp.com/posts")
+      .then((response) => {
+        setSlides([...response.data]);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    autoplay: true,
+    responsive: [
       {
-        title: "Vulputate Mollis Ultricies Fermentum Parturient",
-        description:
-          "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
-        button: "Read More",
-        image: image1,
-        user: "Luan Gjokaj",
-        userProfile: "https://i.imgur.com/JSW6mEk.png",
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
       },
       {
-        title: "Tortor Dapibus Commodo Aenean Quam",
-        description:
-          "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
-        button: "Discover",
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSM5SzNAznPKGcAhoBTQ8ULKCJ20S2ZOE_NxQ&usqp=CAU",
-        user: "Erich Behrens",
-        userProfile: "https://i.imgur.com/0Clfnu7.png",
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
       },
       {
-        title: "Phasellus volutpat metus",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
-        button: "Buy now",
-        image: "https://i.imgur.com/DvmN8Hx.jpg",
-        user: "Bruno Vizovskyy",
-        userProfile: "https://i.imgur.com/4KeKvtH.png",
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
       },
-    ];
-    return (
-      <Router>
-        <header className="header">
-          <a href className="logo">
-            <img
-              src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.jpg"
-              className="logoimage"
-            />
-          </a>
-          <input className="menu-btn" type="checkbox" id="menu-btn" />
-          <label className="menu-icon" htmlFor="menu-btn">
-            <span className="navicon" />
-          </label>
-          <ul className="menu">
-            <li className="contactme">
-              <a href="#work">Contact Me</a>
-            </li>
-            <li className="aboutme">
-              <a href="#about">About Me</a>
-            </li>
-            <li className="projects">
-              <a href="#careers">Projects</a>
-            </li>
-            <li className="Home">
-              <a href="#contact" className="Home">
-                Home
-              </a>
-            </li>
-          </ul>
-        </header>
-        <Slider className="slider-wrapper" autoplay={3000} infinite="true">
-          {content.map((item, index) => (
-            <div
-              key={index}
-              className="slider-content"
-              style={{
-                background: `url('${item.image}') no-repeat center center`,
-              }}
-            >
-              <div className="inner">
-                <h1>{item.title}</h1>
-                <p>{item.description}</p>
-                <button>{item.button}</button>
-              </div>
-              <section>
-                <img src={item.userProfile} alt={item.user} />
-                <span>
-                  Posted by <strong>{item.user}</strong>
-                </span>
-              </section>
+    ],
+  };
+  const content = [
+    {
+      title: "Vulputate Mollis Ultricies Fermentum Parturient",
+      description:
+        "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+      button: "Read More",
+      image: image1,
+      user: "Luan Gjokaj",
+      userProfile: "https://i.imgur.com/JSW6mEk.png",
+    },
+    {
+      title: "Tortor Dapibus Commodo Aenean Quam",
+      description:
+        "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
+      button: "Discover",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSM5SzNAznPKGcAhoBTQ8ULKCJ20S2ZOE_NxQ&usqp=CAU",
+      user: "Erich Behrens",
+      userProfile: "https://i.imgur.com/0Clfnu7.png",
+    },
+    {
+      title: "Phasellus volutpat metus",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+      button: "Buy now",
+      image: "https://i.imgur.com/DvmN8Hx.jpg",
+      user: "Bruno Vizovskyy",
+      userProfile: "https://i.imgur.com/4KeKvtH.png",
+    },
+  ];
+
+  return (
+    <Router>
+      <header className="header">
+        <a href className="logo">
+          <img
+            src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.jpg"
+            className="logoimage"
+          />
+        </a>
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn">
+          <span className="navicon" />
+        </label>
+        <ul className="menu">
+          <li className="contactme">
+            <a href="#work">Contact Me</a>
+          </li>
+          <li className="aboutme">
+            <a href="#about">About Me</a>
+          </li>
+          <li className="projects">
+            <a href="#careers">Projects</a>
+          </li>
+          <li className="Home">
+            <a href="#contact" className="Home">
+              Home
+            </a>
+          </li>
+        </ul>
+      </header>
+      <Slider className="slider-wrapper" autoplay={3000} infinite="true">
+        {content.map((item, index) => (
+          <div
+            key={index}
+            className="slider-content"
+            style={{
+              background: `url('${item.image}') no-repeat center center`,
+            }}
+          >
+            <div className="inner">
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+              <button>{item.button}</button>
             </div>
-          ))}
-        </Slider>
-        <Route path="/posts" exact component={PostsList} />
-        <Route path="/events" exact component={EventsList} />
-        <Route path="/programs" exact component={Program} />
-        <Route path="/galleries" exact component={GalleryList} />
-        <Route path="/posts/:id/comments" exact component={PostAndComments} />
-        <Route
-          path="/programs/:id/programcomments"
-          exact
-          component={ProgramComments}
-        />
-        <Route
-          path="/events/:id/eventcomments"
-          exact
-          component={EventAndComments}
-        />
-      </Router>
-    );
-  }
-}
+            <section>
+              <img src={item.userProfile} alt={item.user} />
+              <span>
+                Posted by <strong>{item.user}</strong>
+              </span>
+            </section>
+          </div>
+        ))}
+      </Slider>
+      <div>
+        <h2> Don't miss these hotel + flight deals</h2>
+        <Slider2 {...settings}>
+          {slides.map((slide, index) => {
+            return (
+              <div key={index}>
+                {/* <ProductCard imgSrc={slide.postImage} /> */}
+                <img
+                  style={{ width: 400, height: 300 }}
+                  src={slide.postImage}
+                  alt={`slide${index}`}
+                />
+                <Card.Body>
+                  <Card.Title style={{ position: "relative", zIndex: 3 }}>
+                    Las Vegas, NV
+                  </Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </div>
+            );
+          })}
+        </Slider2>
+      </div>
+      <Route path="/posts" exact component={PostsList} />
+      <Route path="/events" exact component={EventsList} />
+      <Route path="/programs" exact component={Program} />
+      <Route path="/galleries" exact component={GalleryList} />
+      <Route path="/posts/:id/comments" exact component={PostAndComments} />
+      <Route
+        path="/programs/:id/programcomments"
+        exact
+        component={ProgramComments}
+      />
+      <Route
+        path="/events/:id/eventcomments"
+        exact
+        component={EventAndComments}
+      />
+    </Router>
+  );
+};
 
 export default App;
