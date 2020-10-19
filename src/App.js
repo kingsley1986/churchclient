bfd44c3d4726a1a5d5349cee82f70c9b44af1a89
Kingsley <chukwumakingsley1@gmail.com>
2020-10-19 23:18:55 +0200

    added footer and did some style fixing

--- src/App.js
+++ src/App.js
@@ -245,8 +245,7 @@ export default function App(props) {
                 <div
                   className="row"
                   style={{
-                    marginLeft: -17,
-                    marginRight: -40,
+                    marginLeft: -2,
                   }}
                 >
                   <div
@@ -354,6 +353,151 @@ export default function App(props) {
           onSubmit={saveForm}
         />
       </div>
+
+      <footer>
+        <div>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+            integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+            crossOrigin="anonymous"
+          />
+        </div>
+        <div className="container" className="footer-section">
+          <div className="footer-cta pt-5 pb-5">
+            <div className="row">
+              <div className="col-xl-4 col-md-4 mb-30">
+                <div className="single-cta">
+                  <i className="fas fa-map-marker-alt" />
+                  <div className="cta-text">
+                    <h4>Find us</h4>
+                    <span>1010 Avenue, sw 54321, chandigarh</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-4 col-md-4 mb-30">
+                <div className="single-cta">
+                  <i className="fas fa-phone" />
+                  <div className="cta-text">
+                    <h4>Call us</h4>
+                    <span>9876543210 0</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-4 col-md-4 mb-30">
+                <div className="single-cta">
+                  <i className="far fa-envelope-open" />
+                  <div className="cta-text">
+                    <h4>Mail us</h4>
+                    <span>mail@info.com</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="footer-content pt-5 pb-5">
+            <div className="row">
+              <div className="col-xl-4 col-lg-4 mb-50">
+                <div className="footer-widget">
+                  <div className="footer-logo">
+                    <a href="index.html">
+                      <img
+                        src="https://i.ibb.co/QDy827D/ak-logo.png"
+                        className="img-fluid"
+                        alt="logo"
+                      />
+                    </a>
+                  </div>
+                  <div className="footer-text">
+                    <p>
+                      Lorem ipsum dolor sit amet, consec tetur adipisicing elit,
+                      sed do eiusmod tempor incididuntut consec tetur
+                      adipisicing elit,Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                  <div className="footer-social-icon">
+                    <span>Follow us</span>
+                    <a href="#">
+                      <i className="fab fa-facebook-f facebook-bg" />
+                    </a>
+                    <a href="#">
+                      <i className="fab fa-twitter twitter-bg" />
+                    </a>
+                    <a href="#">
+                      <i className="fab fa-google-plus-g google-bg" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+                <div className="footer-widget">
+                  <div className="footer-widget-heading">
+                    <h3>Useful Links</h3>
+                  </div>
+                  <ul>
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                    <li>
+                      <a href="#">about</a>
+                    </li>
+                    <li>
+                      <a href="#">services</a>
+                    </li>
+                    <li>
+                      <a href="#">portfolio</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact</a>
+                    </li>
+                    <li>
+                      <a href="#">About us</a>
+                    </li>
+                    <li>
+                      <a href="#">Our Services</a>
+                    </li>
+                    <li>
+                      <a href="#">Expert Team</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact us</a>
+                    </li>
+                    <li>
+                      <a href="#">Latest News</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+                <div className="footer-widget">
+                  <div className="footer-widget-heading">
+                    <h3>Subscribe</h3>
+                  </div>
+                  <div className="footer-text mb-25">
+                    <p>
+                      Don’t miss to subscribe to our new feeds, kindly fill the
+                      form below.
+                    </p>
+                  </div>
+                  <div className="subscribe-form">
+                    <form action="#">
+                      <input type="text" placeholder="Email Address" />
+                      <button>
+                        <i className="fab fa-telegram-plane" />
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+
       <Route path="/posts" exact component={PostsList} />
       <Route path="/events" exact component={EventsList} />
       <Route path="/programs" exact component={Program} />
